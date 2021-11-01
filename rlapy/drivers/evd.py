c979dcedc9c5a78c2f5203a8fbed53ca75dba991
rileyjmurray <rjmurray@berkeley.edu>
2021-11-01 11:49:39 -0700

    fix documentation for eigenvalue decomposition drivers

--- rlapy/drivers/evd.py
+++ rlapy/drivers/evd.py
@@ -1,7 +1,8 @@
 import numpy as np
 import scipy.linalg as la
 import rlapy.comps.sketchers.oblivious as oblivious
-from rlapy.comps.rangefinders import RangeFinder, RF1
+import warnings
+from rlapy.comps.rangefinders import RF1
 from rlapy.comps.sketchers.aware import RowSketcher, RS1
 from rlapy.comps.qb import QBDecomposer, QB1
 import rlapy.utils.linalg_wrappers as ulaw
@@ -12,33 +13,37 @@ import rlapy.utils.linalg_wrappers as ulaw
 ###############################################################################
 
 
-def evd1(num_passes, A, k, tol, over, rng):
+def evd1_fr(num_passes, A, k, over, rng):
     """
-    Return the eigen decomposition matrices (V, lamb),
-    based on a rank-k QB factorization of A.
-    Use a Gaussian sketching matrix and pass over A a total of
-    num_passes times.
+    Return ndarrays (V, lamb) that define a symmetric matrix "A_approx" through
+    its eigen-decomposition:
+
+        A_approx = (V * lamb) @ V.T.
+
+    The function assumes A is symmetric.
+    The columns of V are approximations of the dominant eigenvectors of A.
+    The entries of lamb are the corresponding approximate eigenvalues.
+
+    The approximation is "fixed rank." The array V has min(k, rank(A)) columns,
+    and there is no direct control over the approximation error ||A_approx - A||.
+    Increasing "num_passes" and "over" should result in better approximations.
 
     Parameters
     ----------
     num_passes : int
-        Total number of passes over A. We require num_passes >= 2.
+        Total number of passes the algorithm is allowed over A.
+        We require num_passes >= 2.
 
     A : Union[ndarray, spmatrix, LinearOperator]
-        Data matrix to approximate. A must be an n × n Hermitian matrix.
+        Data matrix to approximate. A must be an n × n symmetric matrix.
 
     k : int
-        Target rank for the approximation of A: 0 < k < min(A.shape).
-        This parameter includes any oversampling. For example, if you
-        want to be near the optimal (Eckhart-Young) error for a rank 20
-        approximation of A, then you might want to set k=25.
-        
-    tol : float
-        Target accuracy for the oversampled approximation of A: 0 < tol < np.inf.
-        This parameter inherits from the QBDecomposer or RangeFinder class.
+        Target rank for the approximation of A: 0 < k < n.
         
     over : int
-        Auxiliary parameter for the QBDecomposer or RangeFinder.
+        Perform internal calculations with a sketch of rank (k + over).
+        This is usually a small constant, e.g., 5 to 25. In some situations
+        it's useful to set over = k.
 
     rng : Union[None, int, SeedSequence, BitGenerator, Generator]
         Determines the numpy Generator object that manages randomness
@@ -47,65 +52,67 @@ def evd1(num_passes, A, k, tol, over, rng):
     Returns
     -------
     V : ndarray
-        Has shape (A.shape[0], k). Columns are orthonormal.
+        Has shape (n, min(k, rank(A)). Columns are orthonormal.
 
     lamb : ndarray
-        Has shape (k,), the vector of estimated eigenvalues of A
+        Has shape (min(k, rank(A)),), the vector of estimated eigenvalues of A.
 
     Notes
     -----
     We perform (num_passes - 2) steps of subspace iteration, and
     stabilize subspace iteration by a QR factorization at every step.
+    Subspace iteration is initialized with a Gaussian sketching operator.
 
     References
     ----------
-    This algorithm computes V and then sets B = Q.T @ A. Conceptually, we
-    compute Q by adapting Algorithm 5.3 from
+    This function adapts Algorithm 5.3 from
 
         Halko, Nathan, Per-Gunnar Martinsson, and Joel A. Tropp.
         "Finding structure with randomness: Probabilistic algorithms for
         constructing approximate matrix decompositions."
         SIAM review 53.2 (2011): 217-288.
         (available at `arXiv <http://arxiv.org/abs/0909.4061>`_).
-    EVDecomposer: adapted from the QB-based [HMT11, Algorithm 5.3]
     """
     rng = np.random.default_rng(rng)
     rso_ = RS1(oblivious.SkOpGA(), num_passes - 2, ulaw.orth, 1)
     rf_ = RF1(rso_)
     qb_ = QB1(rf_)
     evd_ = EVD1(qb_)
-    V, lamb = evd_(A, k, tol, over, rng)
+    V, lamb = evd_(A, k, np.NaN, over, rng)
     return V, lamb
 
 
-def evd2(num_passes, A, k, tol, over, rng):
+def evd2_fr(num_passes, A, k, over, rng):
     """
-    Return the eigen decomposition matrices (V, lamb),
-    based on a rank-k QB factorization of A.
-    Use a Gaussian sketching matrix and pass over A a total of
-    num_passes times.
+    Return ndarrays (V, lamb) that define a symmetric positive semidefinite matrix
+    "A_approx" through its eigen-decomposition:
+
+        A_approx = (V * lamb) @ V.T.
+
+    The function assumes A is symmetric positive semidefinite.
+    The columns of V are approximations of the dominant eigenvectors of A.
+    The entries of lamb are the corresponding approximate eigenvalues.
+
+    The approximation is "fixed rank." The array V has min(k, rank(A)) columns,
+    and there is no direct control over the approximation error ||A_approx - A||.
+    Increasing "num_passes" and "over" should result in better approximations.
 
     Parameters
     ----------
     num_passes : int
-        Total number of passes over A. We require num_passes >= 2.
-        
+        Total number of passes the algorithm is allowed over A.
+        We require num_passes >= 2.
+
     A : Union[ndarray, spmatrix, LinearOperator]
-        Data matrix to approximate. A must be an n × n Hermitian PSD matrix.
-        But we did not check PSD
+        Data matrix to approximate. A must be an n × n positive semidefinite matrix.
 
     k : int
-        Target rank for the approximation of A: 0 < k < min(A.shape).
-        This parameter includes any oversampling. For example, if you
-        want to be near the optimal (Eckhart-Young) error for a rank 20
-        approximation of A, then you might want to set k=25.
-        
-    tol = np.nan : NaN by default
-        Target accuracy for the oversampled approximation of A: 0 < tol < np.inf.
-        This parameter inherits from the QBDecomposer or RangeFinder class.
-        
+        Target rank for the approximation of A: 0 < k < n.
+
     over : int
-        Auxiliary parameter for the QBDecomposer or RangeFinder.
+        Perform internal calculations with a sketch of rank (k + over).
+        This is usually a small constant, e.g., 5 to 25. In some situations
+        it's useful to set over = k.
 
     rng : Union[None, int, SeedSequence, BitGenerator, Generator]
         Determines the numpy Generator object that manages randomness
@@ -114,10 +121,10 @@ def evd2(num_passes, A, k, tol, over, rng):
     Returns
     -------
     V : ndarray
-        Has shape (A.shape[0], k). Columns are orthonormal.
+        Has shape (n, min(k, rank(A)). Columns are orthonormal.
 
     lamb : ndarray
-        Has shape (k,). lamb contains the estimated eigenvalues of A.
+        Has shape (min(k, rank(A)),), the vector of estimated eigenvalues of A.
 
     Notes
     -----
@@ -126,19 +133,17 @@ def evd2(num_passes, A, k, tol, over, rng):
 
     References
     ----------
-    This algorithm computes V and then sets B = Q.T @ A. Conceptually, we
-    compute Q by adapting Algorithm 5.3 from
+    This function adapts Algorithm 3 from
 
         Joel A Tropp, Alp Yurtsever, Madeleine Udell, and Volkan Cevher.
         "Fixed-rank approximation of a positive-semidefinite matrix from streaming data."
         Advances in neural information processing systems, 2017.
         (available at `arXiv <https://arxiv.org/abs/1706.05736>`_).
-    EVDecomposer: psd matrices only, [TYUC17a, Algorithm 3]
     """
     rng = np.random.default_rng(rng)
     rso_ = RS1(oblivious.SkOpGA(), num_passes - 2, ulaw.orth, 1)
     evd_ = EVD2(rso_)
-    V, lamb = evd_(A, k, tol, over, rng)
+    V, lamb = evd_(A, k, np.NaN, over, rng)
     return V, lamb
 
 ###############################################################################
@@ -158,34 +163,30 @@ class EVDecomposer:
         Parameters
         ----------
         A : Union[ndarray, spmatrix, LinearOperator]
-            Data matrix to approximate. A must be an n × n Hermitian matrix. 
+            Data matrix to approximate. A must be an n × n symmetric matrix.
 
         k : int
-            Target for the number of columns in Q: 0 < k <= min(A.shape).
-            Typically, k << min(A.shape). Conformant implementations ensure
-            Q has at most k columns. For certain implementations it's
-            reasonable to choose k as large as k = min(A.shape), in which
-            case the implementation returns only once a specified error
-            tolerance has been met.
+            Target rank for the approximation of A: 0 < k < n.
 
         tol : float
-            Target accuracy for the oversampled approximation of A: 0 < tol < np.inf.
-            This parameter inherits from the QBDecomposer or RangeFinder class.
-            
+            Target accuracy for the approximation of A: 0 <= tol < np.inf.
+
         over : int
-            Auxiliary parameter for the QBDecomposer or RangeFinder.
+            Perform internal calculations with a sketch of rank (k + over).
+            This is usually a small constant, e.g., 5 to 25. In some situations
+            it's useful to set over = k.
 
         rng : Union[None, int, SeedSequence, BitGenerator, Generator]
-            Determines the numpy Generator object that manages any and all
-            randomness in this function call.
+            Determines the numpy Generator object that manages randomness
+            in this function call.
 
         Returns
         -------
         V : ndarray
-            Has shape (A.shape[0], k). Columns are orthonormal.
+            Has shape (n, min(k, rank(A)). Columns are orthonormal.
 
         lamb : ndarray
-            Has shape (k,). lamb contains the estimated eigenvalues of A.
+            Has shape (min(k, rank(A)),), the vector of estimated eigenvalues of A.
         """
         raise NotImplementedError()
 
@@ -199,34 +200,31 @@ class EVD1(EVDecomposer):
 
     def __call__(self, A, k, tol, over, rng):
         """
-        Rely on a rangefinder to obtain the matrix Q for the decomposition
-        A \approx Q B. Once we have Q, we construct B = Q.T @ A and return
-        (Q, B). This function is agnostic to the implementation of the
-        rangefinder: it might build a rank-k matrix Q all at once or construct
-        successively larger matrices Q by an iterative process. We make no
-        assumptions on the rangefinder's termination criteria beyond those
-        listed below.
+        Return ndarrays (V, lamb) that define a symmetric matrix "A_approx" through
+        its eigen-decomposition:
+
+            A_approx = (V * lamb) @ V.T.
+
+        The function assumes A is symmetric.
+        The columns of V are approximations of the dominant eigenvectors of A.
+        The entries of lamb are the corresponding approximate eigenvalues.
+
+        This function can accommodate target accuracies if (and only if) the
+        underlying QBDecomposer can accommodate target accuracies.
 
         Parameters
         ----------
-        num_passes : int
-            Total number of passes over A. We require num_passes >= 2.
-
         A : Union[ndarray, spmatrix, LinearOperator]
-            Data matrix to approximate. A must be an n × n Hermitian matrix.
+            Data matrix to approximate. A must be an n × n symmetric matrix.
 
         k : int
             Target rank for the approximation of A: 0 < k < min(A.shape).
-            This parameter includes any oversampling. For example, if you
-            want to be near the optimal (Eckhart-Young) error for a rank 20
-            approximation of A, then you might want to set k=25.
             
         tol : float
             Target accuracy for the oversampled approximation of A: 0 < tol < np.inf.
-            This parameter inherits from the QBDecomposer or RangeFinder class.
             
         over : int
-            Auxiliary parameter for the QBDecomposer or RangeFinder.
+            Perform internal calculations using a sketch of rank k + over.
 
         rng : Union[None, int, SeedSequence, BitGenerator, Generator]
             Determines the numpy Generator object that manages randomness
@@ -235,11 +233,20 @@ class EVD1(EVDecomposer):
         Returns
         -------
         V : ndarray
-            Has shape (A.shape[0], k). Columns are orthonormal.
+            Has shape (n, min(k, rank(A)). Columns are orthonormal.
 
         lamb : ndarray
-            Has shape (k,). lamb contains the estimated eigenvalues of A.
+            Has shape (min(k, rank(A)),). lamb contains the estimated eigenvalues of A.
 
+        Notes
+        -----
+        This function adapts Algorithm 5.3 from
+
+            Halko, Nathan, Per-Gunnar Martinsson, and Joel A. Tropp.
+            "Finding structure with randomness: Probabilistic algorithms for
+            constructing approximate matrix decompositions."
+            SIAM review 53.2 (2011): 217-288.
+            (available at `arXiv <http://arxiv.org/abs/0909.4061>`_).
         """
         assert k > 0
         assert k <= min(A.shape)
@@ -250,16 +257,10 @@ class EVD1(EVDecomposer):
         Q, B = self.qb(A, k + over, tol, rng)
         # B=Q^*A is necessary
         C = B @ Q
-        # d = number of columns in Q, d ≤ k + s
-        d = Q.shape[1]
-        if d > k + over:
-            msg = """
-            This implementation the dimension of Q matrix <= k + s.
-            """ 
-            raise RuntimeError(msg)
-
         lamb, U = la.eigh(C)
         alamb = np.abs(lamb)
+        # d = number of columns in Q, d ≤ k + s
+        d = Q.shape[1]
         r = min(k, d, np.count_nonzero(alamb > np.finfo(float).eps))
         I = np.argsort(-1*np.abs(alamb))[:r]
         # indices of r largest components of |λ|
@@ -271,48 +272,41 @@ class EVD1(EVDecomposer):
 
 class EVD2(EVDecomposer):
 
-    TOL_CONTROL = 'unknown'  # depends on implementation of rangefinder
+    TOL_CONTROL = 'none'
 
-    def __init__(self, rs: RowSketcher):
-        """
-        Parameters
-        ----------
-        rs : RowSketcher
-        """
-        self.rowsketcher = rs
+    def __init__(self, sk_op: RowSketcher):
+        self.sk_op = sk_op
 
     def __call__(self, A, k, tol, over, rng):
         """
-        Rely on a RowSketcher to obtain the matrix S for the sketching matrix
-        S. Once we have S, we construct Y = A @ S and return Cholesky decomposition of 
-        S^T @ Y. This function is agnostic to the implementation of the
-        RowSketcher: it might has different ways of row sketching. We make no
-        assumptions on the RowSketcher's termination criteria beyond those
-        listed below.
+        Return ndarrays (V, lamb) that define a symmetric positive semidefinite matrix
+        "A_approx" through its eigen-decomposition:
 
-        Return the eigen decomposition matrices (V, lamb),
-        based on a row sketched version of A.
-        Use a Gaussian sketching matrix and pass over A a total of
-        num_passes times.
+            A_approx = (V * lamb) @ V.T.
+
+        The function assumes A is symmetric positive semidefinite.
+        The columns of V are approximations of the dominant eigenvectors of A.
+        The entries of lamb are the corresponding approximate eigenvalues.
+
+        The approximation is produced by truncating a rank (k + over) Nystrom
+        approximation
+
+            (A S) (S' A S)^{\\dagger} (A S)'
+
+        to rank k. The matrix S is obtained by S = self.sk_op(A, k + over, rng).
 
         Parameters
         ----------
-        num_passes : int
-            Total number of passes over A. We require num_passes >= 2.
             
         A : Union[ndarray, spmatrix, LinearOperator]
-            Data matrix to approximate. A must be an n × n Hermitian PSD matrix.
-            But we did not check PSD
+            Data matrix to approximate. A must be an n × n symmetric PSD matrix.
+            We do not check that A is PSD (doing so would be too expensive).
 
         k : int
             Target rank for the approximation of A: 0 < k < min(A.shape).
-            This parameter includes any oversampling. For example, if you
-            want to be near the optimal (Eckhart-Young) error for a rank 20
-            approximation of A, then you might want to set k=25.
             
-        tol = np.nan : NaN by default
-            Target accuracy for the oversampled approximation of A: 0 < tol < np.inf.
-            This parameter inherits from the QBDecomposer or RangeFinder class.
+        tol : np.NaN
+            This class cannot control accuracy, and ignores this parameter.
             
         over : int
             Auxiliary parameter for the QBDecomposer or RangeFinder.
@@ -333,14 +327,17 @@ class EVD2(EVDecomposer):
         assert k > 0
         assert k <= min(A.shape)
         if not np.isnan(tol):
-            assert tol >= 0
-            assert tol < np.inf
+            msg = """
+            This EVDecomposer implementation cannot directly control
+            approximation error. Parameter "tol" is being ignored.
+            """
+            warnings.warn(msg)
         rng = np.random.default_rng(rng)
-        S = self.rowsketcher(A, k + over, rng)
+        S = self.sk_op(A, k + over, rng)
         n = A.shape[0]
         Y = A @ S
-        epsilon_mach = np.finfo(float).eps # a temporary regularization parameter
-        nu = np.sqrt(n)*epsilon_mach*la.norm(Y)
+        epsilon_mach = np.finfo(float).eps
+        nu = np.sqrt(n) * epsilon_mach * la.norm(Y)
         # a temporary regularization parameter
         Y = Y + nu*S
         R = la.cholesky(S.T @ Y, lower=True)
