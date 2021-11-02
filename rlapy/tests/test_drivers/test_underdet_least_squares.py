3d3726ac1c13deaaa38b9c0dfecb7c725e50882c
rileyjmurray <rjmurray@berkeley.edu>
2021-11-02 15:00:04 -0700

    proper tests for underdetermined least squares

--- rlapy/tests/test_drivers/test_underdet_least_squares.py
+++ rlapy/tests/test_drivers/test_underdet_least_squares.py
@@ -14,15 +14,16 @@ def make_simple_prob(m, n, spectrum, rng):
     Vt = usk.orthonormal_operator(rank, n, rng)
     A = (U * spectrum) @ Vt
 
-    c = rng.standard_normal(n)
+    y0 = rng.standard_normal(m)
+    c = A.T @ y0
     y_opt = la.lstsq(A.T, c, check_finite=False)[0]
-    ath = AlgTestHelper(A, c, y_opt)
+    ath = AlgTestHelper(A, spectrum, c, y_opt)
     return ath
 
 
 class AlgTestHelper:
 
-    def __init__(self, A, c, y_opt):
+    def __init__(self, A, spectrum, c, y_opt):
         """
         y_opt solves
 
@@ -30,6 +31,7 @@ class AlgTestHelper:
             s.t. A' y = c
         """
         self.A = A
+        self.spectrum = spectrum
         self.c = c
         self.y_opt = y_opt
         self.y_approx = None
@@ -59,7 +61,7 @@ class AlgTestHelper:
         ||A' y - c|| <= tol
         """
         res = self.A.T @ self.y_approx - self.c
-        nrm = la.norm(res, ord=2)
+        nrm = la.norm(res, ord=2) / self.spectrum[0]
         self.tester.assertLessEqual(nrm, tol)
 
     def test_objective(self, tol):
@@ -68,7 +70,8 @@ class AlgTestHelper:
         """
         nrm_approx = la.norm(self.y_approx, ord=2)
         nrm_opt = la.norm(self.y_opt, ord=2)
-        self.tester.assertLessEqual(nrm_approx, nrm_opt + tol)
+        rel_tol = tol * self.spectrum[0]
+        self.tester.assertLessEqual(nrm_approx, nrm_opt + rel_tol)
 
 
 class TestUnderLstsqSolver(unittest.TestCase):
@@ -87,17 +90,65 @@ class TestUnderLstsqSolver(unittest.TestCase):
             ath.test_objective(test_tol)
             ath.test_delta_y(test_tol)
 
+    def run_ath_lowrank(self, ath: AlgTestHelper,
+                              uls: UnderLstsqSolver,
+                              alg_tol, iter_lim,
+                              test_tol, seeds):
+        ath.tester = self
+        for seed in seeds:
+            rng = np.random.default_rng(seed)
+            ath.result = uls(ath.A, ath.c, alg_tol, iter_lim, rng)
+            # skip the check for ||y_opt - y_approx||
+            ath.test_residual(test_tol)
+            ath.test_objective(test_tol)
+
 
 class TestSPU1(TestUnderLstsqSolver):
 
-    #TODO: more tests
-    def test_simple(self):
-        alg = SPU1(oblivious.SkOpSJ(), sampling_factor=3)
+    @staticmethod
+    def default_config():
+        alg = SPU1(
+            sketch_op_gen=oblivious.SkOpSJ(),
+            sampling_factor=3
+        )
+        return alg
 
+    def test_linspace_spec(self):
+        alg = TestSPU1.default_config()
         rng = np.random.default_rng(0)
-        m, n = 1000, 100
-        kappa = 1e5
-        spectrum = np.linspace(kappa ** 0.5, kappa ** -0.5, num=n)
-        ath = make_simple_prob(m, n, spectrum, rng)
 
+        m, n, cond_num = 1000, 100, 1e5
+        spectrum = np.linspace(cond_num ** 0.5, cond_num ** -0.5, num=n)
+
+        ath = make_simple_prob(m, n, spectrum, rng)
         self.run_ath(ath, alg, 1e-12, 50, 1e-6, self.SEEDS)
+
+    def test_logspace_spec(self):
+        alg = TestSPU1.default_config()
+        rng = np.random.default_rng(0)
+
+        m, n, cond_num = 1000, 100, 1e5
+        spec = np.logspace(np.log10(cond_num) / 2, -np.log10(cond_num) / 2, num=n)
+
+        ath = make_simple_prob(m, n, spec, rng)
+        self.run_ath(ath, alg, 1e-12, 50, 1e-6, self.SEEDS)
+
+    def test_higher_accuracy(self):
+        alg = TestSPU1.default_config()
+        rng = np.random.default_rng(0)
+
+        m, n, cond_num = 500, 50, 1e3
+        spectrum = np.linspace(cond_num ** 0.5, cond_num ** -0.5, num=n)
+
+        ath = make_simple_prob(m, n, spectrum, rng)
+        self.run_ath(ath, alg, 0.0, n, 1e-8, self.SEEDS)
+
+    def test_lowrank_linspace_spec(self):
+        alg = TestSPU1.default_config()
+        rng = np.random.default_rng(0)
+
+        m, n, rank, cond_num = 1000, 100, 80, 1e5
+        spectrum = np.linspace(cond_num ** 0.5, cond_num ** -0.5, num=rank)
+
+        ath = make_simple_prob(m, n, spectrum, rng)
+        self.run_ath_lowrank(ath, alg, 1e-12, 50, 1e-6, self.SEEDS)
