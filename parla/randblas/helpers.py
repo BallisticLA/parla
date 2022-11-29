8ce4de4d27f34f4ee2b8333034d0f037f506bdf8
Riley Murray <rjmurray@berkeley.edu>
2022-11-28 16:59:54 -0800

    naming convention

--- parla/randblas/helpers.py
+++ parla/randblas/helpers.py
@@ -12,35 +12,35 @@ def to_2d_array(A_ptr, rows_A, cols_A, lda, layout):
     return A
 
 
-def indexing_bounds(A0_rows, A0_cols, vpoA, A_rows, A_cols, layout):
+def indexing_bounds(A0_rows, A0_cols, poA, A_rows, A_cols, layout):
     # This will be needed in the C++ implementation of RandBLAS.
     #
     #   It identifies row and column index bounds to define a matrix
     #   of shape (A_rows, A_cols), assuming that matrix was formed by
     #   reading a vectorized representation of a matrix of shape
     #   (A0_rows, A0_cols) (vectorization specified by "layout"),
-    #   starting at index "vpoA".
+    #   starting at index "poA".
     #
     if layout == Layout.ColMajor:
-        col_start = vpoA // A0_rows
-        row_start = vpoA % A0_rows
+        col_start = poA // A0_rows
+        row_start = poA % A0_rows
     else:
-        row_start = vpoA // A0_cols
-        col_start = vpoA % A0_cols
+        row_start = poA // A0_cols
+        col_start = poA % A0_cols
     row_end = row_start + A_rows
     col_end = col_start + A_cols
     return (row_start, col_start), (row_end, col_end)
 
 
-def explicit_skop_submatrix(S0_data, vpos, rows_S, cols_S, lds, layout):
+def explicit_skop_submatrix(S0_data, pos, rows_S, cols_S, lds, layout):
     # This is Python-specific.
     if S0_data.ndim == 2:
         (r0, c0), (r1, c1) = indexing_bounds(S0_data.shape[0],
                                              S0_data.shape[1],
-                                             vpos, rows_S, cols_S, layout)
+                                             pos, rows_S, cols_S, layout)
         S = S0_data[r0:r1, c0:c1]
     elif S0_data.ndim == 1:
-        S_data = S0_data[vpos:]
+        S_data = S0_data[pos:]
         S = to_2d_array(S_data, rows_S, cols_S, lds, layout)
     else:
         raise ValueError(f'S0_data.ndim must be 1 or 2, but is {S0_data.ndim}.')
